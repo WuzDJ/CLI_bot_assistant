b42b72945959610ed297fb1b359792d679970b15
WuzDJ <71368963+WuzDJ@users.noreply.github.com>
2023-09-13 13:57:44 +0300

    if call add will be crush

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 contacts = {"bara":"123",
             "ban":"345"}
 
-def hello(): #ok
+def hello():
     return "How can I help you?"
 
 def add(name, phone_num):
-    contacts = {name: phone_num}
+    contacts[name] = phone_num
 
 def change(name, phone_num):
-    contacts = {name: phone_num}
+    if name in contacts.keys():
+        contacts[name] = phone_num
 
 def phone(name):
-    print("{:^10} {:^10}".format(name, contacts[name]))
+    return "{:^10} {:^10}".format(name, contacts[name])
 
-def show_all(): #ok
+def show_all():
     all = []
     for name, phone_num in contacts.items():
         line = "{:^10} {:^10}".format(name, phone_num)
         all.append(line)
     return "\n".join(all)
 
-def good_bye(): #ok
+def good_bye():
     return "Good bye!"
 
 commands = {
@@ -28,8 +29,7 @@ commands = {
     "add": add,
     "change": change,
     "phone": phone,
-    "show all": show_all#,
-    #("good bye", "close", "exit"): good_bye
+    "show all": show_all
 }
 
 def input_error(func):
@@ -42,20 +42,19 @@ def input_error(func):
             print("Value Error")
         except IndexError:
             print("Index Error")
+        except TypeError:
+            print("TypeError")
     return inner
 
-def handler(command):
+def get_handler(command):
     return commands[command]
 
-#@input_error
-def parser(user_input):
-    user_input = user_input.strip()
-    if user_input.lower() in commands:
-        func = handler(user_input.lower())
-    user_input = user_input.split(" ")
-    command = user_input[0]
-    func = handler(command)
-    #func(user_input[1:])
+@input_error
+def handler(func, modifier=None):
+    result = func(*modifier)
+    if result is None:
+        func(*modifier)
+    return result
 
 
 def main():    
@@ -63,12 +62,22 @@ def main():
         user_input = input(">>> ")
         user_input = user_input.strip()
         if user_input.lower() in ("good bye", "close", "exit"):
-            print("Good bye!")
+            print(good_bye())
             break
         if user_input.lower() in commands:
-            print(handler(user_input.lower())())
-        
-        #parser(user_input)
+            print(get_handler(user_input.lower())())
+            continue
+        user_input = user_input.split(" ")
+        command = user_input[0].lower()
+        modifier = user_input[1:]
+        if command in commands:
+            func = get_handler(command)
+            result = handler(func, modifier)
+            if result is not None:
+                print(result)
+            continue
+        else:
+            print("Unknown command")
 
 if __name__ == "__main__":
     main()
